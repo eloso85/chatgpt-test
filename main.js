0841742aff4e552cf855dbc1f5963142808bf8b7
eloso85 <eloso85@gmail.com>
2023-04-30 20:32:39 -0500

    added headers

--- main.js
+++ main.js
@@ -16,14 +16,21 @@ app.listen(port, () => {
 
 
 async function fetchData(){
-    const response = await fetch('https://api.openai.com/v1/chat/completions',{
+    const response = await fetch('https://api.openai.com/v1/completions',{
         method: 'POST',
         headers:{
             Authorization: `Bearer ${process.env.USER_KEY}`,
             "Content-Type": "application/json"
         },
         body: JSON.stringify({
-            
+            model: "text-davinci-003",
+            prompt: " Hello, how are you today",
+            max_token: 7
         })
-    });
+    })
+    const data = await response.json()
+    console.log(data);
 }
+
+fetchData()
+
